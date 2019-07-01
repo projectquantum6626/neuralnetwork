0cf2c4b3cdf17c8983586472d8e78999c5f75bc8
Jonathan Gant <48977353+JonGant@users.noreply.github.com>
2019-07-01 15:06:32 -0400

    Add files via upload

--- DBN/DBN_benchmark.py
+++ DBN/DBN_benchmark.py
@@ -2,9 +2,7 @@
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
-import os.path
-import os.mkdir
-import os.remove
+import os
 import time
 np.random.seed(1337)
 from sklearn.model_selection import train_test_split
@@ -16,7 +14,7 @@ from dbn.tensorflow import SupervisedDBNRegression
 # This function formats a dates dataframe as the day of the year that it is in
 def format_year(df):
     for i, row in df.iterrows():
-        testDate = df.iloc[i,0]
+        testDate = df.iloc[i,1]
         splitDate = testDate.split("/")
         year = int(splitDate[2])
         if year % 4 == 0:
@@ -46,39 +44,23 @@ def format_year(df):
 # number of iterations of backprogagation = int
 # batch size = int
 # activation functions = 'relu'
-def DBN_Run(hidden_layers_struc, learnin_rate_rbm, learnin_rate, num_epochs_rbm, num_iter_backprop, batchSize, activation_func):
-    # This is a parameter for the start date of the model, it must be entered in the format Month/Day/Year like 2/6/2019
-    # The program will find the next closest date to the one listed by iterating forware in the calendar (no leap years)
-    # Enter post 1/3/1950 for Volume data to be there
-    startDate = "6/26/2000"
-
+def DBN_Run(hidden_layers_struc, learnin_rate_rbm, learnin_rate, num_epochs_rbm, num_iter_backprop, batchSize, activation_func, SP_Data):
     # defines a start time for the project job
     startTime = time.time()
-    # Creates the output file director if it is not already created
-    if not os.path.isdir("benchmark-output"):
-        os.mkdir("benchmark-output")
-
-    # Loading dataset
-    SP_Data_Full = pd.read_csv("data-files/S&P_Movement.csv", sep=',')
-
-    # Change start index based on the given start date and read in new dataframe
-    startIndex = SP_Data_Full.index[SP_Data_Full["Date"] == startDate].values
-    SP_Data = SP_Data_Full.iloc[startIndex[0]:-1]
-    SP_Data.reset_index(inplace = True)
 
     # Shift the data set so that the model is reading in the previous day's information on the High, Low, Close, Volume
     # and Movement
-    SP_Data["PrevHigh"] = SP_Data["High"].shift(-1)
-    SP_Data["PrevLow"] = SP_Data["Low"].shift(-1)
-    SP_Data["PrevClose"] = SP_Data["Close"].shift(-1)
-    SP_Data["PrevVolume"] = SP_Data["Volume"].shift(-1)
-    SP_Data["PrevMovement"] = SP_Data["Movement"].shift(-1)
-
+    SP_Data_Shifted = SP_Data.shift(1)
     # split the new dataframe into features and targets
-    target = SP_Data[["Close"], ["Movement"]]
-    features = SP_Data[["Date", "Open", "PrevHigh", "PrevLow", "PrevVolume", "PrevMovement"]]
-
-    features = format_year(features)
+    target = SP_Data[["Close", "Movement"]]
+    features = SP_Data[["Date", "Open"]]
+    features = pd.concat([features, SP_Data_Shifted[["High", "Low", "Volume", "Movement"]]], axis=1)
+    print("SP_Data: " + "\n")
+    print(SP_Data)
+    print("SP_Data_Shifted: " + "\n")
+    print(SP_Data_Shifted)
+    print("features: " + "\n")
+    print(features)
 
     # format the dataframe as a numpy array for the tensorflow functions
     target_array = target.values
@@ -92,6 +74,7 @@ def DBN_Run(hidden_layers_struc, learnin_rate_rbm, learnin_rate, num_epochs_rbm,
     min_max_scaler = MinMaxScaler()
     X_train = min_max_scaler.fit_transform(X_train)
 
+    print(X_train)
     # Training
     regressor = SupervisedDBNRegression(hidden_layers_structure=hidden_layers_struc,
                                         learning_rate_rbm=learnin_rate_rbm,
@@ -155,25 +138,41 @@ def DBN_Run(hidden_layers_struc, learnin_rate_rbm, learnin_rate, num_epochs_rbm,
 # activation function: will try all of these different benchmarks on all the different activation functions
 if os.path.exists("data-files/reset.csv"):
     reset_df = pd.read_csv("data-files/reset.csv")
-    activation_func = reset_df.at[0, "activation_func"]
+    # Creates the output file director if it is not already created
+    if not os.path.isdir("benchmark-output"):
+        os.mkdir("benchmark-output")
+    activation_func = reset_df.iat[0, 6]
+    # Loading dataset
+    SP_Data_Full = pd.read_csv("data-files/S&P_Movement.csv", sep=',')
+    # This is a parameter for the start date of the model, it must be entered in the format Month/Day/Year like 2/6/2019
+    # The program will find the next closest date to the one listed by iterating forware in the calendar (no leap years)
+    # Enter post 1/3/1950 for Volume data to be there
+    startDate = "6/26/2000"
+    # Change start index based on the given start date and read in new dataframe
+    startIndex = SP_Data_Full.index[SP_Data_Full["Date"] == startDate].values
+    SP_Data = SP_Data_Full.iloc[startIndex[0]:-1]
+    SP_Data.reset_index(inplace=True)
+    SP_Data = format_year(SP_Data)
     if activation_func == "relu":
-        for learnRate in range(reset_df.at[0, "learnRate"], 101):
-            for learnRateRBM in range(reset_df.at[0, "learnRateRBM"], 101):
-                for hiddenlayerNum in range(reset_df.at[0, "hiddenlayerNum"], 101):
-                    for hiddenlayerNumNodes in range(reset_df.at[0, "hiddenlayerNumNodes"], 101):
-                        for numEpochs in range(reset_df.at[0, "numEpochs"], 11):
-                            for backpropNum in range(reset_df.at[0, "backpropNum"], 101):
+        for learnRate in range(reset_df.iat[0, 0], 101):
+            for learnRateRBM in range(reset_df.iat[0, 1], 101):
+                for hiddenlayerNum in range(reset_df.iat[0, 2], 101):
+                    for hiddenlayerNumNodes in range(reset_df.iat[0, 3], 101):
+                        for numEpochs in range(reset_df.iat[0, 4], 11):
+                            for backpropNum in range(reset_df.iat[0, 5], 101):
                                 pool = mp.Pool(10)
                                 job_list = []
                                 for batchSize in range(1, 11):
                                     job_list.append(([hiddenlayerNumNodes, hiddenlayerNum], learnRateRBM / 1000,
                                                      learnRate / 1000, numEpochs * 10, backpropNum * 10, batchSize * 10,
-                                                     activation_func))
+                                                     activation_func, SP_Data))
                                 pool.starmap(DBN_Run, job_list)
                                 pool.close()
                                 pool.join()
                                 # write the reset file
-                                file = open("data-files/reset.csv", "w")
+                                file = open("data-files/reset.csv", "w+")
+                                file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,"
+                                           + "backpropNum,activation_func\n")
                                 file.write(str(learnRate) + "," + str(learnRateRBM) + "," + str(hiddenlayerNum) + "," +
                                            str(hiddenlayerNumNodes) + "," + str(numEpochs) + "," + str(backpropNum) + "," +
                                            str(activation_func) + "\n")
@@ -191,12 +190,14 @@ if os.path.exists("data-files/reset.csv"):
                                 for batchSize in range(1, 11):
                                     job_list.append(([hiddenlayerNumNodes, hiddenlayerNum], learnRateRBM / 1000,
                                                      learnRate / 1000, numEpochs * 10, backpropNum * 10,
-                                                     batchSize * 10, activation_func))
+                                                     batchSize * 10, activation_func, SP_Data))
                                 pool.starmap(DBN_Run, job_list)
                                 pool.close()
                                 pool.join()
                                 # write the reset file
-                                file = open("data-files/reset.csv", "w")
+                                file = open("data-files/reset.csv", "w+")
+                                file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,"
+                                           + "backpropNum,activation_func\n")
                                 file.write(
                                     str(learnRate) + "," + str(learnRateRBM) + "," + str(hiddenlayerNum) + "," +
                                     str(hiddenlayerNumNodes) + "," + str(numEpochs) + "," + str(backpropNum) + "," +
@@ -205,23 +206,25 @@ if os.path.exists("data-files/reset.csv"):
         if os.path.exists("data-files/reset.csv"):
             os.remove("data-files/reset.csv")
     else:
-        for learnRate in range(1, 101):
-            for learnRateRBM in range(1, 101):
-                for hiddenlayerNum in range(1, 101):
-                    for hiddenlayerNumNodes in range(1, 101):
-                        for numEpochs in range(1, 11):
-                            for backpropNum in range(1, 101):
+        for learnRate in range(reset_df.iat[0, 0], 101):
+            for learnRateRBM in range(reset_df.iat[0, 1], 101):
+                for hiddenlayerNum in range(reset_df.iat[0, 2], 101):
+                    for hiddenlayerNumNodes in range(reset_df.iat[0, 3], 101):
+                        for numEpochs in range(reset_df.iat[0, 4], 11):
+                            for backpropNum in range(reset_df.iat[0, 5], 101):
                                 pool = mp.Pool(10)
                                 job_list = []
                                 for batchSize in range(1, 11):
                                     job_list.append(([hiddenlayerNumNodes, hiddenlayerNum], learnRateRBM / 1000,
                                                      learnRate / 1000, numEpochs * 10, backpropNum * 10,
-                                                     batchSize * 10, activation_func))
+                                                     batchSize * 10, activation_func, SP_Data))
                                 pool.starmap(DBN_Run, job_list)
                                 pool.close()
                                 pool.join()
                                 # write the reset file
-                                file = open("data-files/reset.csv", "w")
+                                file = open("data-files/reset.csv", "w+")
+                                file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,"
+                                           + "backpropNum,activation_func\n")
                                 file.write(
                                     str(learnRate) + "," + str(learnRateRBM) + "," + str(hiddenlayerNum) + "," +
                                     str(hiddenlayerNumNodes) + "," + str(numEpochs) + "," + str(backpropNum) + "," +
@@ -230,12 +233,22 @@ if os.path.exists("data-files/reset.csv"):
         if os.path.exists("data-files/reset.csv"):
             os.remove("data-files/reset.csv")
 else:
-    # create the reset file
-    file = open("data-files/reset.csv", "w+")
-    file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,backpropNum,activation_func\n")
-    file.close()
+    # Creates the output file directory if it is not already created
+    if not os.path.isdir("benchmark-output"):
+        os.mkdir("benchmark-output")
     # Activation function of relu
     activation_func = 'relu'
+    # Loading dataset
+    SP_Data_Full = pd.read_csv("data-files/S&P_Movement.csv", sep=',')
+    # This is a parameter for the start date of the model, it must be entered in the format Month/Day/Year like 2/6/2019
+    # The program will find the next closest date to the one listed by iterating forware in the calendar (no leap years)
+    # Enter post 1/3/1950 for Volume data to be there
+    startDate = "6/26/2000"
+    # Change start index based on the given start date and read in new dataframe
+    startIndex = SP_Data_Full.index[SP_Data_Full["Date"] == startDate].values
+    SP_Data = SP_Data_Full.iloc[startIndex[0]:-1]
+    SP_Data.reset_index(inplace=True)
+    SP_Data = format_year(SP_Data)
     for learnRate in range(1, 101):
         for learnRateRBM in range(1, 101):
             for hiddenlayerNum in range (1, 101):
@@ -246,12 +259,15 @@ else:
                             job_list = []
                             for batchSize in range(1, 11):
                                 job_list.append(([hiddenlayerNumNodes, hiddenlayerNum], learnRateRBM/1000,
-                                                 learnRate/1000, numEpochs*10, backpropNum*10, batchSize*10, activation_func))
+                                                 learnRate/1000, numEpochs*10, backpropNum*10, batchSize*10,
+                                                 activation_func, SP_Data))
                             pool.starmap(DBN_Run, job_list)
                             pool.close()
                             pool.join()
                             # write the reset file
-                            file = open("data-files/reset.csv", "w")
+                            file = open("data-files/reset.csv", "w+")
+                            file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,"
+                                       + "backpropNum,activation_func\n")
                             file.write(str(learnRate) + "," + str(learnRateRBM) + "," + str(hiddenlayerNum) + "," +
                                        str(hiddenlayerNumNodes) + "," + str(numEpochs) + "," + str(backpropNum) + "," +
                                        str(activation_func) + "\n")
@@ -268,12 +284,15 @@ else:
                             job_list = []
                             for batchSize in range(1, 11):
                                 job_list.append(([hiddenlayerNumNodes, hiddenlayerNum], learnRateRBM/1000,
-                                                 learnRate/1000, numEpochs*10, backpropNum*10, batchSize*10, activation_func))
+                                                 learnRate/1000, numEpochs*10, backpropNum*10, batchSize*10,
+                                                 activation_func, SP_Data))
                             pool.starmap(DBN_Run, job_list)
                             pool.close()
                             pool.join()
                             # write the reset file
-                            file = open("data-files/reset.csv", "w")
+                            file = open("data-files/reset.csv", "w+")
+                            file.write("learnRate,learnRateRBM,hiddenlayerNum,hiddenlayerNumNodes,numEpochs,"
+                                       + "backpropNum,activation_func\n")
                             file.write(str(learnRate) + "," + str(learnRateRBM) + "," + str(hiddenlayerNum) + "," +
                                        str(hiddenlayerNumNodes) + "," + str(numEpochs) + "," + str(backpropNum) + "," +
                                        str(activation_func) + "\n")
