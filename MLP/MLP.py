875d9739a4b0e6b9605e84ac2662b7cba3361fa6
Alain Garcia <alaingarcia@ufl.edu>
2019-07-03 17:17:46 -0400

    Updated MLP

--- MLP/MLP.py
+++ MLP/MLP.py
@@ -10,12 +10,10 @@ import os
 from joblib import dump, load # model persistence
 
 # Parameters for S&P 500
-data_file = '../input/processed/S&P.csv'
-output_col = 'Movement'
+data_file, output_col, hidden_layer = '../input/processed/S&P.csv', 'Movement', (8,6)
 
 # Parameters for banknote authentication data set
-#data_file = '../input/banknote_authentication.csv'
-#output_col = 'Class'
+#data_file, output_col, hidden_layer = '../input/banknote_authentication.csv', 'Class', (8,4)
 
 df = pd.read_csv(data_file, sep=',')
 
@@ -33,7 +31,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Define MLP model and train
-classifier=MLPClassifier(hidden_layer_sizes=(8,4), max_iter=500, alpha=0.0001,
+classifier=MLPClassifier(hidden_layer_sizes=hidden_layer, max_iter=500, alpha=0.0001,
                             solver='sgd', verbose=10,  random_state=21,tol=0.000001)
 classifier.fit(X_train,Y_train)
 
