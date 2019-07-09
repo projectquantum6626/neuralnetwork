268a58fb80b507e1c7d8a1fb058fe642bd09c26b
Alain Garcia <alaingarcia@ufl.edu>
2019-07-09 16:22:33 -0400

    Added more to Everything.csv, filled empty spots

--- constantReplacement.py
+++ constantReplacement.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('input/SILVER_USD_DAILY_3.csv', sep=',')
-# Date column contains the dates that the data came with
-originalDays = df['Date'].values
-
-# Date2 column contains the dates that I'm interested in
-allDays = df['Date2'].values
-
-# Bid column contains the prices
-bid = df['Bid'].values
 
 #inserts into a column and pushes everything below downwards
 def insert(df, column, row, value):
@@ -21,21 +12,52 @@ def remove(df, column, row):
     indeces = df.iloc[row:].index.tolist()
     df.loc[indeces,[column]] = df.loc[indeces,[column]].shift(-1)
 
-#removes dates available in original days that are not desired in all days
-for i in range(len(originalDays)):
-    while (df['Date'].iloc[i] not in allDays) and not (pd.isna(df['Date'].iloc[i])):
-        remove(df, 'Date', i)
-        remove(df, 'Bid', i)
+# fills in empty spots in Everything.csv
+def fillInEmpty():
+        everything_df = pd.read_csv('input/processed/Everything.csv', sep=',')
+        for column in everything_df:
+                print("Filling "+column)
+                for i in range(len(everything_df[column].values)):
+                        row = everything_df[column].iloc[i]
+                        if (pd.isna(row) or row == 'ND' or row == 'NA' or
+                        row == '' or row == ' ' or row == '.'):
+                                everything_df[column].iloc[i] = everything_df[column].iloc[i-1]
+        everything_df.to_csv('input/processed/Everything2.csv', index=False)
 
-#inserts dates that aren't originally there (constant replacement)
-for i in range(len(allDays)):
-    while (df['Date2'].iloc[i] not in originalDays) and not (pd.isna(df['Date'].iloc[i])):
-        insert(df, 'Date', i, df['Date2'].iloc[i])
-        insert(df, 'Bid', i, df['Bid'].iloc[i-1])
 
-#inserts prices that aren't originally there (constant replacement)
-for i in range(len(bid)):
-    if pd.isna(df['Bid'].iloc[i]) and not pd.isna(df['Date'].iloc[i]):
-        df['Bid'].iloc[i] = df['Bid'].iloc[i-1]
-    
-df.to_csv('input/processed/SILVER_USD_DAILY_P.csv', index=False)
+# format an individual file (add missing dates, remove extra dates)
+def individualFile(filename):
+        df = pd.read_csv('input/'+filename, sep=',')
+        everything_df = pd.read_csv('input/processed/Everything.csv', sep=',')
+
+        everything_df = everything_df['Date']
+        df = pd.concat((df, everything_df.rename('Everything_Date')), axis=1)
+
+        # Date column contains the dates that the data came with
+        originalDays = df['Date'].values
+
+        # Everything's date column contains the dates that I'm interested in
+        allDays = df['Everything_Date'].values
+
+        columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+        #removes dates available in original days that are not desired in all days
+        for i in range(len(originalDays)):
+                while (df['Date'].iloc[i] not in allDays) and not (pd.isna(df['Date'].iloc[i])):
+                        remove(df, 'Date', i)
+                        for col in columns:
+                                remove(df, col, i)
+
+        #inserts dates that aren't originally there (constant replacement)
+        for i in range(len(allDays)):
+                while (df['Everything_Date'].iloc[i] not in df['Date'].values):
+                        insert(df, 'Date', i, df['Everything_Date'].iloc[i])
+                        for col in columns:
+                                insert(df, col, i, df[col].iloc[i-1])
+        #inserts prices that aren't originally there (constant replacement)
+        """for i in range(len(bid)):
+        if pd.isna(df['Bid'].iloc[i]) and not pd.isna(df['Date'].iloc[i]):
+                df[''].iloc[i] = df[''].iloc[i-1]"""
+        df = df.drop(columns=['Everything_Date'])
+        df.to_csv('input/processed/P_'+filename, index=False)
+
+fillInEmpty()
