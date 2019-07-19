fe23013db09690dec3bd9edc2610a93664fc297a
Alain Garcia <alaingarcia@ufl.edu>
2019-07-19 14:55:55 -0400

    added SVM

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -3,17 +3,25 @@ Dependencies:
 pip install matplotlib statsmodels numpy pandas sklearn ploty keras
 """
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn import svm
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
+from datetime import datetime
 import os
 from joblib import dump, load # model persistence
 
 #if (not os.path.exists(output_col + '_MLP.joblib')):
 # Parameters for banknote authentication data set
 #data_file, output_col, hidden_layer = '../input/banknote_authentication.csv', 'Class', (8,4)
-def classifyMLP(data_file, output_col, hidden_layer, solve, period=None):
+def classifySVM(data_file, output_col, period=None, Date=False):
     df = pd.read_csv(data_file, sep=',')
+
+    if not Date:
+        df = df.drop(columns=['Date'])
+    else:
+        df['Date'] = pd.to_datetime(df['Date'])
+        df['Date'] = [datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S").timestamp() for x in df['Date']]
 
     # Define inputs and outputs
     X = df.drop(columns=[output_col]).values
@@ -29,9 +37,11 @@ def classifyMLP(data_file, output_col, hidden_layer, solve, period=None):
     X_test = scaler.transform(X_test)
 
     # Define MLP model and train
-    classifier=MLPClassifier(hidden_layer_sizes=hidden_layer, max_iter=20000, alpha=0.00001,
-                                solver=solve, verbose=10,  random_state=21, tol=0.0000001)
-    classifier.fit(X_train,Y_train)
+    for kernel in ['rbf', 'poly', 'linear']:
+        classifier = svm.SVC(kernel=kernel, gamma='auto')
+        classifier.fit(X_train,Y_train.ravel())
+        predicted = classifier.predict(X_test)
+        print('{} {}'.format(kernel, accuracy_score(Y_test, predicted)))
 
     """dump(classifier, output_col + '_MLP.joblib')
         print('Dumped model')
@@ -40,14 +50,11 @@ def classifyMLP(data_file, output_col, hidden_layer, solve, period=None):
         print('Loaded model')
         classifier = load(output_col + '_MLP.joblib')"""
 
-    # Print results
-    accuracies=cross_val_score(estimator=classifier,X=X_test,y=Y_test,cv=10)
+    #log = open('log.csv', 'a+')
+    #log.write(data_file.replace('../input/processed/everything/','') + '\n')
 
-    
-    log = open('log.csv', 'a+')
-    log.write(data_file.replace('../input/processed/everything/','') + '\n')
-    #log.write("Accuracies: {}".format(accuracies))
-    log.write(solve+',{}\n'.format(accuracies.mean()))
+# folder = '../input/processed/everything/'
+classifySVM('../input/processed/everything/Everything_delta.csv', 'S&P_Movement')
 
 # FULL 19 years
 # classifyMLP('../input/processed/everything/Everything.csv', 'S&P_Movement', (3,6), 'sgd')
@@ -61,9 +68,6 @@ for period in [1,2,3,4,5,6,7,8,9]:
             if file.endswith('old.csv'):
                 classifyMLP(folder+'/'+file, 'S&P_Movement', (3,6), solver, period)
 '''
-folder = '../input/processed/everything/'
-for solver in ['lbfgs','sgd','adam']:
-    classifyMLP(folder+'P_Everything.csv', 'S&P_Movement', (3,6), solver)
 
 # Just 1 year periods
 '''
