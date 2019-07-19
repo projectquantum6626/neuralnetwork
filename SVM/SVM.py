90558f0b03c7dc47c3e8fec296aa29aca3a0bad0
Alain Garcia <alaingarcia@ufl.edu>
2019-07-19 17:07:03 -0400

    Normalized data

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -3,6 +3,7 @@ Dependencies:
 pip install matplotlib statsmodels numpy pandas sklearn ploty keras
 """
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
 from sklearn import svm
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score
@@ -10,14 +11,14 @@ from sklearn.preprocessing import StandardScaler
 from datetime import datetime
 import os
 
-def classifySVM(data_file, output_col, period=None, Date=False):
+def classifySVM(data_file, output_col, period=None, Date=False, processed=False):
     df = pd.read_csv(data_file, sep=',')
 
     # Choosing whether to take in Date column or not
     if not Date:
         if 'Date' in df.columns:
             df = df.drop(columns=['Date'])
-    else:
+    elif not processed:
         df['Date'] = pd.to_datetime(df['Date'])
         df['Date'] = [datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S").timestamp() for x in df['Date']]
 
@@ -29,11 +30,12 @@ def classifySVM(data_file, output_col, period=None, Date=False):
     X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.3,random_state=1)
 
     # Scale everything
-    scaler = StandardScaler()
+    """scaler = StandardScaler()
     X = scaler.fit_transform(X)
     X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    X_test = scaler.transform(X_test)"""
 
+    #X_train = np.nan_to_num(X_train)
     # Define MLP model and train
     for kernel in ['rbf', 'poly', 'linear']:
         classifier = svm.SVC(kernel=kernel, gamma='auto')
@@ -44,5 +46,5 @@ def classifySVM(data_file, output_col, period=None, Date=False):
 # Test with popular data set
 # classifySVM('../input/banknote_authentication.csv', 'Class')
 
-classifySVM('../input/processed/everything/Everything_delta.csv', 'S&P_Movement')
+classifySVM('../input/processed/everything/Everything_delta_normalized.csv', 'S&P_Movement')
 
